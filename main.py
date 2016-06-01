fbf8a22d5f86473da2a3b37abdc328c7b805b9eb
Donnie Tolbert <donnie.tolbert@target.com>
2016-06-01 06:41:14 -0500

    more

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ class GoPiGoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(s):
         if s.path == "/poll":
             s.send_response(200)
+            s.send_header("Content-Type", "text/plain")
+            s.end_headers()
             s.wfile.write("volt %s\n" % gopigo.volt())
             s.wfile.write("firmware %s\n" % gopigo.fw_ver())
         elif s.path == "/beep":
             s.send_response(200)
+            s.send_header("Content-Type", "text/plain")
+            s.end_headers()
         elif s.path == "/reset_all":
             s.send_response(200)
+            s.send_header("Content-Type", "text/plain")
+            s.end_headers()
         else:
             s.send_error(404, "Unknown path %s" % s.path)
 
