fc6b81f44bd53ff8d43ff5a6ba0e95fca02bfbaf
Donnie Tolbert <donnie.tolbert@target.com>
2016-06-01 06:29:37 -0500

    first

---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+import BaseHTTPServer
+
+HOST_NAME = ""
+PORT_NUMBER = 8080
+
+
+class GoPiGoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(s):
+        if s.path == "/poll":
+            s.wfile.write("volt 12.5\n")
+            s.send_response(200)
+        elif s.path == "/beep":
+            s.send_response(200)
+        elif s.path == "/reset_all":
+            s.send_response(200)
+        else:
+            s.send_error(404, "Unknown path %s" % s.path)
+
+if __name__ == "__main__":
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), GoPiGoHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
